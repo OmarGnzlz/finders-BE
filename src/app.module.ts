ace7fda3ca32ea770dee94e1137c8cf46e3e684d
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2021-02-20 00:37:48 -0600

    Removing files not used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [LoginModule, AuthModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
