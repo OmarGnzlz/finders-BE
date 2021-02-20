7e3acd79b97d83024ac59964581fd4f3f0287d50
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2021-02-20 00:58:08 -0600

    Adding information module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LoginModule } from './login/login.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { InformationModule } from './information/information.module';
 
 @Module({
-  imports: [LoginModule, AuthModule, DatabaseModule],
+  imports: [LoginModule, AuthModule, DatabaseModule, InformationModule],
   controllers: [],
   providers: [],
 })
