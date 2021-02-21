9f97cdb9ef2b566712a8b45ebf020e22e1ca0ca0
Gabospa <gabospa@gmail.com>
2021-02-21 03:54:06 -0500

    <update> / Creation of Register module

---
+++ src/register/register.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRegisterDto } from './dto/create-register.dto';
+
+@Injectable()
+export class RegisterService {
+
+    create(createRegisterDto: CreateRegisterDto) {
+        return 'This action creates new register';
+    }
+}
