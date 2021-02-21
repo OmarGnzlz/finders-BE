b286aec3025bd7084773bc0059351dc75f5fdeda
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2021-02-20 22:47:00 -0600

    Adding configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import * as Joi from '@hapi/joi'
+
 import { LoginModule } from './login/login.module';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './database/database.module';
 import { InformationModule } from './information/information.module';
-import { ReferenceModule } from './database/reference/reference.module';
 
 @Module({
-  imports: [LoginModule, AuthModule, DatabaseModule, InformationModule, ReferenceModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) =>
+        config.get<TypeOrmModuleOptions>(TYPEORM_CONFIG),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        configSecret
+      ],
+      envFilePath: `.env`,
+      validationSchema: Joi.object({ 
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development')
+      }),
+    }),
+    LoginModule, 
+    AuthModule, 
+    InformationModule, 
+  ],
   controllers: [],
   providers: [],
 })
