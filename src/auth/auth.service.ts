eacc5977af9d04d74482c8b63048b8fbe75be013
CarGDev <ingecarlos.gutierrez@gmail.com>
2021-02-21 18:03:08 -0600

    Creating bases for jwt authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService
+  ) {}
+
+  async login(user: any): Promise<any> {
+    try {
+      const payload = { username: user.username };
+      return this.jwtService.sign(payload);
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
